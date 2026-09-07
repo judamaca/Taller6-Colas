f93489103e30830e401c542e47208d54d376b9d0
judamaca <juandavidmateus123@hotmail.com>
2026-09-06 19:41:05 -0500

    Archivos Base

---
+++ QueueInterface.java
@@ -0,0 +1,21 @@
+import java.util.Iterator;
+
+public interface QueueInterface {
+
+    /*
+    essential operations
+    */
+    public void clear();
+
+    public boolean isEmpty();
+
+    public Object extract();
+
+    public boolean insert(Object object);
+
+    public int size();
+
+    public boolean search(Object object);
+
+    public String toString();
+}
